2dbac0200eeeb7ed378f8d4db2312d65c3f4703e
KimY <yungookim@gmail.com>
2013-09-14 03:37:15 -0400

    merging

--- server/public/app/scripts/main.js
+++ server/public/app/scripts/main.js
@@ -1,45 +1,39 @@
-// /*global public, $*/
+$(document).ready(function () {
+  // 'use strict';
 
-// window.public = {
-//   Models: {},
-//   Collections: {},
-//   Views: {},
-//   Routers: {},
-//   init: function () {
-//     'use strict';
-//   }
-// };
+  // window.sync = [
 
-// $(document).ready(function () {
-//   // 'use strict';
-//   public.init();
+  // ];
 
-//   window.positions = {};
 
-//   $.post('/config/' + window.id, 
-//     {width: $(window).width(), height:$(window).height()},
-//     function(ret){
-//       console.log(ret);
-//   });
 
-//   $(".rect").animate({left : $(window).width() + "px"}, 
-//     1000, "linear", 
 
-//     function() {
+
+
+  // $.post('/config/' + window.id, 
+  //   {width: $(window).width(), height:$(window).height()},
+  //   function(ret){
+  //     console.log(ret);
+  // });
+
+  // $(".rect").animate({left : $(window).width() + "px"}, 
+  //   1000, "linear", 
+
+  //   function() {
     
-//       var position = {
-//         top : $(".rect").position().top,
-//         left : $(".rect").position().left,
-//         statue : 'done'
-//       };
+  //     var position = {
+  //       top : $(".rect").position().top,
+  //       left : $(".rect").position().left,
+  //       statue : 'done'
+  //     };
 
-//       $.post('/position/' + window.id, 
-//           position,
-//           function(ret){
-//             console.log(positions);
-//           }
-//       );
-//     }
-//   );
+  //     $.post('/position/' + window.id, 
+  //         position,
+  //         function(ret){
+  //           console.log(positions);
+  //         }
+  //     );
+  //   }
+  // );
 
-// });
+});
