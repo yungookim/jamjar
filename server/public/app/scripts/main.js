5522e1080ced3949ea5e9785cde0355aa8f9bf82
KimY <yungookim@gmail.com>
2013-09-14 07:29:25 -0400

    audio

--- server/public/app/scripts/main.js
+++ server/public/app/scripts/main.js
@@ -6,17 +6,46 @@ $(document).ready(function () {
   window.state = -1;
   //myDataRef.set(0);
   window.myDataRef.on('value', function(snapshot) {
-    console.log(snapshot.val());
+    // console.log(snapshot.val());
     window.state = snapshot.val();
 
-    if (window.id === 0){
-    // 
 
-    } else if (window.id === 1) {
+    window.soundInt = setInterval(function(){
 
-    } else if (window.id === 3) {
+      if (window.id === 0){
+      // t9
+        if ($('body').hasClass('t9')){
+          window.Sound.setVolume(100);
+        } else {
+          window.Sound.setVolume(0);
+        }
+      } else if (window.id === 1) {
+      // t10
+        if ($('body').hasClass('t10')){
+          window.Sound.setVolume(100);
+        } else {
+          window.Sound.setVolume(0);
+        }
+      } else if (window.id === 3) {
+        // t11
+        if ($('body').hasClass('t11')){
+          window.Sound.setVolume(100);
+        } else {
+          window.Sound.setVolume(0);
+        }
+      }
 
-    }
+      if ($('body').hasClass('t12')){
+        window.Sound.setVolume(0);
+        window.clearInterval(window.soundInt);
+      }
+
+    }, 1000);
+
+
+    
+
+    // t12 stop everything
 
     if (window.state > -1) {
       $('body').addClass('t' + window.state);  
